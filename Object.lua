692043f954ef59d39075dcc21e87e27d4df9acd3
icyselec <icyselec@gmail.com>
2024-08-28 08:18:29 +0900

    Embedded the basic usage of the library.

--- Object.lua
+++ Object.lua
@@ -1,16 +1,74 @@
--- SPDX-License-Identifier: LGPL-2.1-only
--- luajit-object
--- Original author: icyselec
--- Part of SILO Project
+---	SPDX-License-Identifier: LGPL-2.1-only
 
 local isSupportedGc = not newproxy
 local debug = (not isSupportedGc) and require 'debug'
 
+---	Origin of all objects.
+------
+---	This section only describes the fields; the description of the methods is in the beginning of each definition.
+------
+---## function Object.final (self)
+---	This field may not exist, `self` must have been derived from `Object`.
+---
+---	Defines the behavior when an instance is destroyed.
+---	This function shall work equally on any platform.
+---	This function does not necessarily need to be defined.
+------
+---## function Object.__gc (self)
+---	`self` can be `Object` or `userdata`.
+---
+---	For each instance, this metamethod will be invoked automatically upon destruction;
+---	therefore, it shall not be overriden or called arbitrarily.
 ---@class Object
 ---@field final? fun(self: self)
 local Object = {}
 
----@generic T
+---@param self Object | userdata
+local function gc (self)
+	self = (type(self) == 'userdata') and getmetatable(self) or self ---@cast self Object
+
+	-- If not a instance, then return immediately.
+	if rawget(self, 'new') then return end
+
+	---@type Object?
+	local base = getmetatable(self)
+
+	while base do
+		if base.final then base.final(self) end
+
+		base = getmetatable(base) --[[@as Object?]]
+	end
+end
+
+local function sealed (_, _) error("Attempt to instantiate a sealed object.") end
+
+---	Instantiate the object.
+------
+---	When an instance overrides a constructor, it becomes a type.
+---	You can create an instance as a datatype by writing it as follows.
+---	---@class Object
+---	local Object = require 'Object'
+---
+---	---@class Type: Object
+---	---@field name  string
+---	---@field info? string
+---	local Type = Object:new()
+---
+---	--- You must override the constructor for each data type.
+---	function Type:new (o)
+---		o = self:super(o)
+---
+---		-- your custom constructor here, this is example.
+---		o.name = o.name or "NullType"
+---		o.info = o.info or "NullType is just a nil."
+---
+---		return o
+---	end
+---
+---	From now on, Type will provide type information. (if LSP is installed)
+---	local SomeType = Type:new{ info = "SomeType sometimes become NoneType.", } -- Error: missing-fields
+---	local NoneType = Type:new{ info = "NoneType sometimes become SomeType.", name = 0, } -- Error: assign-type-mismatch
+---@generic T: Object
 ---@param self T
 ---@param o any
 ---@return T
@@ -20,8 +78,7 @@ function Object:new (o)
 	o = o or {}
 
 	if not getmetatable(o) then
-		self.__gc = self.__gc
-		o.__gc = self.__gc
+		self.__gc, o.__gc = gc, gc
 
 		if not isSupportedGc then
 			o[debug.setmetatable(newproxy(false), o)] = not nil
@@ -38,47 +95,34 @@ end
 
 Object.super = Object.new
 
----@generic T
----@param self T
----@return T
+---	Seal the instance.
+------
+---	so that it is no longer derived.
+---@return self
 ---@nodiscard
-function Object:static ()
-	self.new = self.new or Object.new
-	return self
-end
+function Object:sealed () return rawset(self, 'new', sealed) end
 
+---	Find out which type the object belongs to.
+------
+---	It can be used as follows.
+---* Examine which type inherits a particular type: SomeType:typeof(Object) -- is SomeType inherit Object?
+---* Instance check: print(Type:typeof(Type), Instance:typeof(Instance)) -- it prints "true, false"
 ---@param that any
 ---@return boolean
-function Object:instanceof (that)
+function Object:typeof (that)
 	that = that or Object
 
-	if not rawget(self, 'new') then
+	if rawget(self, 'new') then
 		repeat
 			if self == that then return true end
 
 			self = getmetatable(self)
 		until not self
+
+		error("Type is unreachable.")
 	end
 
 	return false
-end
-
----@private
----@param self Object | userdata
-function Object:__gc ()
-	self = (type(self) == 'userdata') and getmetatable(self) or self ---@cast self Object
-
-	-- If not a instance, then return immediately.
-	if rawget(self, 'new') then return end
-
-	---@type Object
-	local base = getmetatable(self)
-
-	while base do
-		if base.final then base.final(self) end
-
-		base = getmetatable(base) --[[@as Object]]
-	end
 end
 
 return Object
